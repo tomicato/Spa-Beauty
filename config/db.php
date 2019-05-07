120611d5042622ed9b9018dad6a0f83f58177cd3
Alexey <sfinks134@gmail.com>
2019-05-08 01:57:28 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=relation',
+    'dsn' => 'mysql:host=localhost;dbname=test',
     'username' => 'root',
-    'password' => '',
+    'password' => '1234',
     'charset' => 'utf8',
 ];
